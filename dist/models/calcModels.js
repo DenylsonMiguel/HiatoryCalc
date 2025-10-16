b61704ff4ab2ccee335ffd6cc1f02cb349688e50
Denylson <denylsongpereira@gmail.com>
2025-10-16 03:48:23 -0300

    Delete dist directory

--- dist/models/calcModels.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-const calcSchema = new Schema({
-    operation: { type: String, required: true },
-    result: { type: Number, required: true },
-    creation: { type: Date, default: Date.now },
-});
-export const CalcModel = model("Calc", calcSchema);
-//# sourceMappingURL=calcModels.js.map
